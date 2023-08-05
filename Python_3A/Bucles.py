5bb96169a7422dd6d0e74842223e69fd011bbb39
al3-rom <sasha.stankevich223@gmail.com>
2023-08-05 18:56:33 +0200

    Añadiendo ejercicios

---
+++ Python_3A/Bucles.py
@@ -0,0 +1,21 @@
+# Pedir al usuario un numero entero y mostar por pantalla una estructura
+# como esta: 
+# *
+# **
+# ***
+# ****
+# *****
+# ****
+# ***
+# **
+# *
+# Donde el valor de entrada es el numero de estrellas en el centro de la estructura
+
+# Pedimos el numero y convertimos en un numero entero: ( int() )
+num = int(input("Pon un numero: "))
+
+# Hacemos el bucle
+ #No se como hacer
+    
+
+
---
+++ README.md
@@ -0,0 +1,5 @@
+Hello! I'm going to leave my course exercises solved here! List and bucle
+
+Hola! Aqui voy a dejar mis ejercicios del curso resueltos! Listas y bucles
+
+Привет, я оставлю здесь решённые задачи по Python! Списки и циклы
