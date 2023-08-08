6b7d1967bc6187156b23b9c740d75373f0d95bba
al3-rom <sasha.stankevich223@gmail.com>
2023-08-08 18:15:47 +0200

    Actualizando ejercicios

---
+++ Python_3A/PillandoSoltura.py
@@ -0,0 +1,16 @@
+# 1.Escribir una programa para encontrar los elementos duplicados de una lista,
+#  añadirlos a nueva lista y borrarlos de la lista. Despues imprimir una lista con tan solo los elementos unicos
+# 2.Unir dos listas y ordenarlas en orden ascendente 
+# 3.Encontrar el segundo numero mas grande da la lista
+# 4.Contar el numero de elementos mas grandes que un determinado numero dado por el usuario(supon una lista numerica)
+# 5.Crear un script dado un numero introducido por el usuario o determinado al inicio del programa,
+# realizar la suma de aquellos numeros que sean dividibles por este 
+# 6.Pedir un numero al usuario y dada una lista encontrar el numero mas alto que es inferior al numero introducido
+# o determinado al inicio del programa
+# 7.Crear un script que extraiga los elementos comunes entre dos listas
+# 8.Crear un script para contar el numero de apariciones de un elemento de una lista en dicha lista(P.e en la lista =[23,65,23] el numero de apariciones de 23 es 2 )
+# 9.Escribir un programa que lea una lista de enteros y cree una nueva lista que contenga solo numeros positivos de la lista original
+# 10.Tomar una lista de strings y crear una nueva lista que contenga el tamaño de los strings de la lista original
+# 11.Crear un programa que dada una lista de strings, devuelva otra lista con los strings en mayuscula
+
+#NO LO SE
