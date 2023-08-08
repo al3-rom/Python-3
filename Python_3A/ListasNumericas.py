0dcf0cd9eb33b7ccb911620355081197023ea86c
al3-rom <sasha.stankevich223@gmail.com>
2023-08-08 19:34:47 +0200

    Repaso con profe

--- Python_3A/ListasNumericas.py
+++ Python_3A/ListasNumericas.py
@@ -8,27 +8,35 @@
 # 8.Encontrar el indice correspondiente al numero 8 en la lista original y en la lista resultante tras el punto 2
 
 # 1
-numeros = [1,2,3,4,5,6,7,8,9,10]
+#numeros = [1,2,3,4,5,6,7,8,9,10]
+numeros = list(range(1,11))
+print(numeros)
+print( " / / / ") #Para separar las listas
 # 2
-newList = []
+pares_invertidos = []
 
 for numero in reversed(numeros):
     if numero % 2 == 0:
-        newList.append(numero)
+        pares_invertidos.append(numero)
+# [::-1] - para inverso(en vez de reversed)
 
+print(pares_invertidos)
+print( " / / / ") #Para separar las listas
 # 3
+
 for numero in numeros:
-    print(numero)
+    print(numero**2)
 print( " / / / ") #Para separar las listas
 
 # 4
-# no se
-
+pares_invertidos = [numero for numero in numeros if numero % 2 == 0][::-1]
+numero_cuadrados = [numero**2 for numero in numeros]
+print(pares_invertidos, numero_cuadrados)
 # 5
-print(min(numeros))
+print(min(numeros)) #Numero minimo
 print( " / / / ") #Para separar las listas
 # 6
-print(max(numeros))
+print(max(numeros))# Numero maximo
 print( " / / / ") #Para separar las listas
 # 7
 
@@ -45,4 +53,6 @@ print(sumaTotal)
 print( " / / / ") #Para separar las listas
 
 # 8 
-# No se
+indice_8_orig = numeros.index(8)
+indice_8_invert = pares_invertidos.index(8)
+print(indice_8_orig, ",",indice_8_invert)
--- Python_3A/PillandoSoltura.py
+++ Python_3A/PillandoSoltura.py
@@ -13,4 +13,134 @@
 # 10.Tomar una lista de strings y crear una nueva lista que contenga el tamaño de los strings de la lista original
 # 11.Crear un programa que dada una lista de strings, devuelva otra lista con los strings en mayuscula
 
-#NO LO SE
+# 1
+
+lista = [1,2,3,4,5,6,3,7,8,9,8,10]
+print("Lista original:", lista)
+elementos_duplicados = []
+elementos_unicos = []
+
+for elemento in lista:
+    #count para contar elementos
+    if lista.count(elemento) > 1:
+        if elemento not in elementos_duplicados:
+         elementos_duplicados.append(elemento)
+    else:
+       elementos_unicos.append(elemento)
+
+for elemento in elementos_duplicados:
+   lista.remove(elemento)       
+print("Elementso unicos son:", elementos_unicos)
+print("Elementos duplicados:", elementos_duplicados)
+print("Lista nueva:", lista)
+
+print( "---------------") # Para separar los ejercicios!
+# 2
+lista1 = [3,4,1,5]
+lista2 = [6,2,7,8]
+
+lista_combinada = lista1 + lista2
+lista_combinada.sort()
+print(lista_combinada)
+
+
+print( "---------------") # Para separar los ejercicios!
+
+# 3
+numbers = [3,4,1,5,6,2,7,8]
+numbers.sort(reverse=True)
+print(numbers[1])
+
+print( "---------------") # Para separar los ejercicios!
+# 4
+
+numeros = [3,4,1,5,6,2,7,8]
+numero_objetivo = 4
+contador = 0
+for num in numeros:
+   if num > numero_objetivo:
+      contador +=1
+print(contador)
+
+print( "---------------") # Para separar los ejercicios!
+# 5
+
+numeros = [3,4,1,5,6,2,7,8]
+
+divisor = 3
+resultado = 0
+for num in numeros:
+   if num % divisor == 0:
+      resultado +=num
+print(resultado)
+print( "---------------") # Para separar los ejercicios!
+
+# 6 
+
+numeros = [3,4,1,5,6,2,7,8]
+numero_objetivo = 4
+numeros.sort()
+
+for num in numeros:
+   if num < numero_objetivo:
+      resultado = num
+print(resultado)
+print( "---------------") # Para separar los ejercicios!
+
+# 7
+
+lista1 = [1,2,3,4,5,6]
+lista2 = [4,5,6,7,8,9]
+
+comunes =[]
+
+for elemento in lista1:
+   if elemento in lista2:
+      comunes.append(elemento)
+print("Elementos comunes", comunes)
+print( "---------------") # Para separar los ejercicios!
+
+# 8
+
+numeros = [23,65,23]
+objetivo = 23
+print(numeros.count(objetivo))
+
+#otro ejemplo:
+#for elemento in numeros:
+   #print(f"El elemento {elemento}, aparece {numeros.count(elemento)} veces en la lista")
+
+print( "---------------") # Para separar los ejercicios!
+# 9
+
+lista_original = [-1,-2,-3,4,-5,6,-7,8,-9]
+lista_positiva = []
+
+for numero in lista_original:
+   if numero > 0:
+      lista_positiva.append(numero)
+print("Lista original:", lista_original)
+print("Lista positivos:", lista_positiva)
+print( "---------------") # Para separar los ejercicios!
+
+# 10
+lista_strings = ["hola", "estas", "usando", "python"]
+lista_longitudes = []
+
+for string in lista_strings:
+   longitud = len(string)
+   lista_longitudes.append(longitud)
+print("Lista de strings", lista_strings)
+print("Lista de longitudes", lista_longitudes)
+print( "---------------") # Para separar los ejercicios!
+
+# 11
+
+lista_strings = ["hola", "estas", "usando", "python"]
+lista_mayuscula = []
+
+for string in lista_strings:
+   lista_mayuscula.append(string.upper())
+print("Lista de strings", lista_strings)
+print("Lista en matuscula", lista_mayuscula)
+print( "---------------") # Para separar los ejercicios!
