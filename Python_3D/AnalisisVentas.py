847251716df16471aff79344be67cda1d0387f4e
al3-rom <sasha.stankevich223@gmail.com>
2023-08-17 17:27:32 +0200

    Actualizando ejercicios

---
+++ Python_3D/AnalisisVentas.py
@@ -0,0 +1,7 @@
+# Hay una lista de ventas de los ultimos 30 dias. Queremos analizar las ventas por dia de la semana para identificar los dias de mayor venta
+
+ventas = [120,25,11,134,234,134,125,521,522,932,145,254,342,134,324,154,643,253,267,356,322,643,321,111,39,653,233,133,333,375]
+
+dias_semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+
---
+++ Python_3D/ValidacionAcceso.py
@@ -0,0 +1,3 @@
+# Crear un script que verifique si el nombre de usuario y la 
+# contraseña ingresados son correctos y permita el acceso solo si ambos son correctos
+
