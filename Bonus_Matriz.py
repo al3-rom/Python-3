f5f083816f6e571a5e8d8d92c3d2a16f326934b8
al3-rom <sasha.stankevich223@gmail.com>
2023-08-22 19:30:12 +0200

    Repaso con profe

--- Bonus_Matriz.py
+++ Bonus_Matriz.py
@@ -1,7 +1,7 @@
 # Crear un script que dada una lista de listas M(numerica), identifique si se trata de una matriz y en ese caso 
 # imprima dos listas correspondientes a:
 # 1.La fila cuyos elementos suman el maximo
-# 2.La columna cuyos elementos sumal el maximo
+# 2.La columna cuyos elementos suman el maximo
 # Si no se trata de una matriz devolvera dos listas vacias
 
 # ejemplo: 
@@ -9,3 +9,75 @@
 # M2 = [[4,2,3], [4,5], [6,8,2]] devolvera L1 = [] y L2 = []
 
 # Definimos matriz como una lista de listas donde todas las listas internas tienen el mismo numero de objetos
+
+# Definir la lista de listas M
+M = [[2,5,3],
+     [6,1,8],
+     [7,5,4]]
+
+# Verificar si M es una matriz
+
+n_columnas = len(M[0])
+n_filas = len(M)
+es_matriz = True
+# Recorrer todas las listas dentro de  M(Todas las filas de M)
+
+for i in range(0, n_filas):
+    # Compruebo si la fila i tienen la misma dimension
+    # que la de referencia
+    if len(M[i]) != n_columnas: # Si no tiene el mismo numero de columnas
+        es_matriz = False # Si alguna tiene un numero de elementos distinto
+                            # a la de referencia, entonces es_matriz sera falso
+        break
+
+
+# Parte 1
+#Si es una matriz, ejecutamos
+ 
+if es_matriz :
+    sum_max = 0
+    # recorrer las filas con un bucle
+    for i in range(0,n_filas):
+        fila = M[i] # Cada una de las filas
+        suma_fila = sum(fila) # calculamos la suma de la fila
+        # comprobamos si la suma de la fila es
+        if suma_fila > sum_max:
+            sum_max = suma_fila # actualizamos el valor de la suma maxima
+            num_fila = i        # actualizamos el valor del indice de la
+                                # fila con la suma mas alta
+            
+    print("La fila cuyos elementos suman el maximo es:", M[num_fila], "con una suma total de:", sum_max)
+
+# Parte 2
+""" 
+M = [[2,5,3],
+     [6,1,8],
+     [7,5,4]]
+
+"""
+# Si es una matriz, ejecutamos
+
+if es_matriz:
+    suma_max = 0 # inicializamos la variable que guarda la suma maxima
+    # recorremos todas las columnas de la matriz
+    for j in range(0, n_columnas):
+        columna = [] # inicializamos nuestra lista donde guardamos los valores
+                     # de cada una de las columnas en cada iteracion
+        suma_columna = 0 # inicializamos la variable que contiene la suma de la columna
+
+        # recorrer las filas de la matriz
+        for fila in M:
+            suma_columna = suma_columna + fila[j] # sumar cada elemento de la columna j
+            columna.append(fila[j]) # añadir cada elemento de la columna j a nuestra lista columna
+            # comprobar si la suma de la columna es mayor a la suma de la columna anterior
+        print(columna, suma_columna)
+        if suma_columna > suma_max: 
+            suma_max = suma_columna # actualizamos el valor de la suma maxima
+            columna_max = columna[:] # actualizamos el valor de la columna con suma maxima
+            print("Resultado if:", columna_max, suma_max)
+
+    print("La columna cuyos elementos suman el maximo es:", columna_max, "con una suma total de:", suma_max)
+            
+        
+
+        
--- Bonus_StringAlista.py
+++ Bonus_StringAlista.py
@@ -6,9 +6,36 @@ introducirlo en una lista de listas donde se encuentra la información de todos 
 imprimir la nota media de los alumnos junto con el DNI.
 Supón ahora que tu input es un string como este:
 "David Fernandez 12311267A 43527 2 9.1 7.6 2.4\n
-Maria Garcia 12316487A 43527 2 7.1 8.6 5.4\n
-Juan Perez 647829236A 43527 2 8.1 8.5 8.4\n "
 Reescribe el script para que procese ese input adecuadamente e imprima la nota media y el DNI
 de todos los alumnos en ese string. 
 
 """
+# Lista de listas
+base_datos = [["Alvaro", "Gomez", "87654372B", "64782", "1", "7.6", "5.4", "9.3"]]
+
+# Definir la cadena de caracteres con la informacion de un alumno
+cadena = "David Fernandez 12311267A 43527 2 2.1 4.6 3.4"
+
+# convertir la cadena de entrada en una lista
+datos_alumno = cadena.split() # split separara la cadena en una lista de valores individuales
+
+
+# introducir la lista con los datos del alumno en la base de datos
+base_datos.append(datos_alumno)
+
+
+for alumno in base_datos:
+    
+    suma_notas = 0
+    n_notas = 0
+
+    dni = alumno[2] # extraemos el DNI del alumno
+    # calculamos la media del alumno
+    for i in range(5, len(alumno)):
+        suma_notas = suma_notas + float(alumno[i]) 
+        n_notas = n_notas + 1 # calculamos el numero total de notas del alumno
+
+    nota_media = suma_notas / n_notas # calculamos la nota media del alumno
+
+    print(f"El alumno con dni: {dni}, tiene una nota media de:{nota_media:.2f}") # :.2f-sirve para que nos muestra 2 decimales al final(ejemplo: 2.03211 = 2.03)
+
